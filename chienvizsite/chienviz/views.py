39442e117f25b3eaed2a3c0abaf82f1bd23cbe28
gyasui <go04jp@yahoo.co.jp>
2018-07-20 18:37:46 +0900

    minor change

--- chienvizsite/chienviz/views.py
+++ chienvizsite/chienviz/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import datetime, json
 from .models import Tweet, TrainTrouble
+import requets
 
 # Create your views here.
 
